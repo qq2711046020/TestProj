34b77ecf5f317a484cca3dc4c8cd990b635b9b2f
chenjunhao <2711046020@qq.com>
2022-07-21 00:58:07 +0800

    Add Unlua

---
+++ Content/Script/Map/FirstMap.lua
@@ -0,0 +1,38 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+require "UnLua"
+
+---@class FirstMap
+local FirstMap = Class()
+
+--function FirstMap:Initialize(Initializer)
+--end
+
+--function FirstMap:UserConstructionScript()
+--end
+
+--function FirstMap:ReceiveBeginPlay()
+--end
+
+--function FirstMap:ReceiveEndPlay()
+--end
+
+-- function FirstMap:ReceiveTick(DeltaSeconds)
+-- end
+
+--function FirstMap:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+--end
+
+--function FirstMap:ReceiveActorBeginOverlap(OtherActor)
+--end
+
+--function FirstMap:ReceiveActorEndOverlap(OtherActor)
+--end
+
+return FirstMap
