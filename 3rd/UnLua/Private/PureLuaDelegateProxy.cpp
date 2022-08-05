5b56fa0d503bc3481374277aa3a13cb09583a542
陈君豪 <2711046020@qq.com>
2022-08-05 19:14:58 +0800

    add test folder

---
+++ 3rd/UnLua/Private/PureLuaDelegateProxy.cpp
@@ -0,0 +1,1 @@
+#include "PureLuaDelegateProxy.h"
---
+++ 3rd/UnLua/Private/PureLuaDelegateProxy.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/Object.h"
+#include "PureLuaDelegateProxy.generated.h"
+
+UCLASS(BlueprintType)
+class UPureLuaDelegateProxy : public UObject
+{
+	GENERATED_BODY()
+};
